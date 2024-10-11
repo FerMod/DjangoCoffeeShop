2924cbd0e1b4a942d89b59d9a1b9ee45c0538beb
FerMod <tudelaigarcia@gmail.com>
2024-10-11 11:55:31 +0200

    Add product list

--- products/urls.py
+++ products/urls.py
@@ -18,8 +18,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from products.views import ProductFormView
+from products.views import ProductFormView, ProductListView
 
 urlpatterns = [
+    path("", ProductListView.as_view(), name="product_list"),
     path("add/", ProductFormView.as_view(), name="add_product"),
 ]
--- products/views.py
+++ products/views.py
@@ -2,15 +2,25 @@ from django.urls import reverse_lazy
 from django.views import generic
 
 from products.forms import ProductForm
+from products.models import Product
 
 
 # Create your views here.
 class ProductFormView(generic.FormView):
-    template_name = 'products/add_product.html'
+    template_name = "products/add_product.html"
     form_class = ProductForm
-    success_url = reverse_lazy('add_product')
+    success_url = reverse_lazy("add_product")
 
     def form_valid(self, form):
         form.save()
         return super().form_valid(form)
-    
+
+
+class ProductListView(generic.ListView):
+    template_name = "products/product_list.html"
+    model = Product
+    context_object_name = "products"
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
