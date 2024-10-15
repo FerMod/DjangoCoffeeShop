3ef12be4ac3fce456192cb11ba7152fa52f32861
FerMod <tudelaigarcia@gmail.com>
2024-10-15 10:14:49 +0200

    Add tests

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from django.urls import reverse
+
+from products.models import Product
 
 # Create your tests here.
+class ProductListViewTests(TestCase):
+
+    def test_returns_200(self):
+        url = reverse('product_list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context['products'].count(), 0)
+
+    def test_returns_200_with_product(self):
+        url = reverse('product_list')
+        Product.objects.create(
+            name='test',
+            description='test description',
+            price=2.8,
+            available=True,
+        )
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context['products'].count(), 1)
